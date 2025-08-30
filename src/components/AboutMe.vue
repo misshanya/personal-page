f90aba58147fd9727eb63d45d03b0a534e4a13e5
misshanya <mishanyadev@gmail.com>
2025-08-30 22:39:40 +0300

    feat: use whileInView to animate only current items

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,32 +1,15 @@
 <script setup lang="ts">
 import { motion } from 'motion-v';
 import { ref } from 'vue';
-import { useIntersectionObserver } from '@vueuse/core';
-
-const aboutContainer = ref(null)
-const isAboutVisible = ref(false)
-
-useIntersectionObserver(
-    aboutContainer,
-    ([{ isIntersecting }]) => {
-        if (isIntersecting) {
-            isAboutVisible.value = true;
-        }
-    }
-)
 </script>
 
 <template>
     <div
-        class="container"
-        ref=aboutContainer
-    >
-        <div class="about"
-        v-if="isAboutVisible"
-        >
+        class="container">
+        <div class="about">
             <motion.div
             :initial='{ opacity: 0, y: 20 }'
-            :animate="{ opacity: 1, y: 0 }"
+            :whileInView="{ opacity: 1, y: 0 }"
             :transition="{ duration: 1.5, delay: 0.2, type: 'spring' }"
             >
                 <p>I write backend in <img src="/go.svg"> language</p>
@@ -34,7 +17,7 @@ useIntersectionObserver(
 
             <motion.div
             :initial='{ opacity: 0, y: 20 }'
-            :animate="{ opacity: 1, y: 0 }"
+            :whileInView="{ opacity: 1, y: 0 }"
             :transition="{ duration: 1.5, delay: 0.4, type: 'spring' }"
             >
                 <p>I really love complex systems</p>
@@ -42,7 +25,7 @@ useIntersectionObserver(
 
             <motion.div
             :initial='{ opacity: 0, y: 20 }'
-            :animate="{ opacity: 1, y: 0 }"
+            :whileInView="{ opacity: 1, y: 0 }"
             :transition="{ duration: 1.5, delay: 0.6, type: 'spring' }"
             >
                 <p>I use <img src="/linux.svg"> on all of my devices</p>
